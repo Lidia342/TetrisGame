ad79e2e5315067bebb1138c4159ab21fca97c9ca
Lidia342 <lidiaabraham0@gmail.com>
2022-03-26 01:43:16 +0100

    Speed/scroll/bugs

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const wid = 10
     let nextRandom = 0
     let score = 0
+    var timerId;
+    var number; 
     const colors = [
         'orange',
         'red',
@@ -14,7 +16,57 @@ document.addEventListener('DOMContentLoaded', ()=>{
         'blue'
       ]
 
-  //The Tetrominoes
+      //move down funciton 
+  function moveDown(){
+    undraw()
+    currentPosition += wid
+    draw()
+    freeze()
+} 
+
+      $('#easy').click(function () { 
+        draw();
+       timerId = setInterval(moveDown, 2000);
+        nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+        displayShape();
+          number = 2000; 
+          $("#fast").hide();
+          $("#fastest").hide();
+
+                document.getElementById("fast").disabled = true;
+          document.getElementById("fastest").disabled = true;
+    
+    
+      });
+      $('#fast').click(function (e) { 
+        draw();
+    timerId = setInterval(moveDown, 1000);
+        nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+        displayShape();
+        number = 1000; 
+    
+        $("#easy").hide();
+        $("#fastest").hide();
+          document.getElementById("easy").disabled = true;
+        document.getElementById("fastest").disabled = true;
+    
+      });
+      $('#fastest').click(function (e) { 
+        draw();
+     timerId =  setInterval(moveDown,500);      
+        nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+        displayShape();
+        number = 500; 
+        $("#fast").hide();
+        $("#easy").hide();
+        document.getElementById("fast").disabled = true;
+        document.getElementById("easy").disabled = true;
+    
+        
+      }); 
+
+      
+  //Tetrominoes
   const lTetromino = [
     [1, wid+1, wid*2+1, 2],
     [wid, wid+1, wid+2, wid*2+2],
@@ -61,7 +113,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
 
 
-  //draw first rotation in the first tetromino
+  
   function draw() {
     current.forEach(index => {
       squares[currentPosition + index].classList.add('tetromino')
@@ -70,7 +122,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     })
   }
   
-  // undraw tetromino
+
   function undraw(){
       current.forEach(index => {
         squares[currentPosition + index].classList.remove('tetromino')
@@ -78,7 +130,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       })
   }
 
-  timerId= setInterval(moveDown, 1000);
+ 
 
    //assign functions to keyCodes
    function control(e) {
@@ -95,15 +147,9 @@ document.addEventListener('DOMContentLoaded', ()=>{
 
   document.addEventListener('keyup', control)
 
-  //move down funciton 
-  function moveDown(){
-      undraw()
-      currentPosition += wid
-      draw()
-      freeze()
-  }
+ 
 
-    //freeze function
+   
     function freeze() {
       if(current.some(index => squares[currentPosition + index + wid].classList.contains('taken'))) {
         current.forEach(index => squares[currentPosition + index].classList.add('taken'))
@@ -143,7 +189,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
   draw()
 }
 
-//rotate the tetromino
+
 function rotate() {
   undraw()
   currentRotation ++
@@ -157,9 +203,9 @@ function rotate() {
 
 const displaySquares = document.querySelectorAll('.mini-grid div')
 const displayWid = 4
+let displayIndex = 0
 
-
-//the Tetrominos without rotations
+//without rotations
 const upNextTetrominoes = [
   [1, displayWid+1, displayWid*2+1, 2], //lTetromino
   [0, displayWid, displayWid+1, displayWid*2+1], //zTetromino
@@ -168,21 +214,38 @@ const upNextTetrominoes = [
   [1, displayWid+1, displayWid*2+1, displayWid*3+1] //iTetromino
 ]
 
-
-        //add functionality to the button
-  startBtn.addEventListener('click', () => {
-    if (timerId) {
-      clearInterval(timerId)
-      timerId = null
-    } else {
-      draw()
-      timerId = setInterval(moveDown, 1000)
-      nextRandom = Math.floor(Math.random()*theTetrominoes.length)
-      //displayShape()
-    }
+ //display the shape in the mini-grid display
+ function displayShape() {
+  //remove any trace of a tetromino form the entire grid
+  displaySquares.forEach(square => {
+    square.classList.remove('tetromino')
+    square.style.backgroundColor = ''
   })
+  upNextTetrominoes[nextRandom].forEach( index => {
+    displaySquares[displayIndex + index].classList.add('tetromino')
+    displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
+  })
+}
 
-  //add score
+startBtn.addEventListener('click', () => {
+
+  var currentvalue = document.getElementById('start-button').value;
+
+  if(currentvalue == "start"){
+  clearInterval(timerId); 
+  timerId = null;   
+  document.getElementById("start-button").value="pause";
+  }
+  else{
+    document.getElementById("start-button").value="start";
+    draw();
+    timerId = setInterval(moveDown, number)
+     nextRandom = Math.floor(Math.random()*theTetrominoes.length)
+     displayShape();
+  }
+})
+
+
   function addScore() {
     for (let i = 0; i < 199; i +=wid) {
       const row = [i, i+1, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
@@ -202,19 +265,34 @@ const upNextTetrominoes = [
     }
   }
 
- /* if(isStorage && localStorage.getItem('fap-scores').split(','))*/
 
-
-
-    //game over
 function gameOver() {
 if(current.some(index => squares[currentPosition + index].classList.contains('taken'))) {
   scoreDisplay.innerHTML = 'end'
- /* isStorage && localStorage.setItem('fap-scores', elements.socres)*/
   clearInterval(timerId)
+
 }
 }
 
  
 
 })
+window.addEventListener('keydown', (e) => {
+  if (e.target.localName != 'input') {   // if you need to filter <input> elements
+      switch (e.keyCode) {
+          case 37: // left
+          case 39: // right
+              e.preventDefault();
+              break;
+          case 38: // up
+          case 40: // down
+              e.preventDefault();
+              break;
+          default:
+              break;
+      }
+  }
+}, {
+  capture: true,   // this disables arrow key scrolling in modern Chrome
+  passive: false   // this is optional, code works without it
+});
